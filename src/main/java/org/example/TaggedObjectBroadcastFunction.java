fa082f58505f51ef7c7e7d420615ac064f8effb2
Evgeny Cheremushkin <evgeny.cheryomushkin@gmail.com>
2023-02-12 20:52:18 +0700

    working version that use broadcast state

--- src/main/java/org/example/TaggedObjectBroadcastFunction.java
+++ src/main/java/org/example/TaggedObjectBroadcastFunction.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.runtime.state.KeyedStateFunction;
 import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 
@@ -23,13 +22,7 @@ public class TaggedObjectBroadcastFunction extends KeyedBroadcastProcessFunction
 
     @Override
     public void processBroadcastElement(Control value, KeyedBroadcastProcessFunction<String, TaggedObject, Control, TaggedObject>.Context ctx, Collector<TaggedObject> out) throws Exception {
-        log.warn("broadcast ("+value+")");
-        ctx.applyToKeyedState(valueStateDescriptor, new KeyedStateFunction<String, ValueState<TaggedObject>>() {
-            @Override
-            public void process(String key, ValueState<TaggedObject> state) throws Exception {
-                out.collect(state.value());
-            }
-        });
+        ctx.applyToKeyedState(valueStateDescriptor, (key, state) -> out.collect(state.value()));
     }
 
     @Override
